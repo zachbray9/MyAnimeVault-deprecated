d904c778d992fd8a9eef1d183687e2795b85d56f
Zachary Bray <zachbray9@knights.ucf.edu>
2023-11-02 04:12:27 -0400

    Created UI for account menu in navbar
    Created the UI for the dropdown menu in the nav bar. It currently drops down to display the account settings and logout options, but they haven't been implemented yet.

--- MyAnimeVault/MyAnimeVault/wwwroot/js/site.js
+++ MyAnimeVault/MyAnimeVault/wwwroot/js/site.js
@@ -2,3 +2,23 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+document.addEventListener("click", e => {
+    const isDropdownButton = e.target.matches("[data-dropdown-button]");
+
+    if (!isDropdownButton && e.target.closest('[data-dropdown]') != null)
+        return
+
+    let CurrentDropdown
+    if (isDropdownButton)
+    {
+        CurrentDropdown = e.target.closest('[data-dropdown]')
+        CurrentDropdown.classList.toggle('active')
+    }
+
+    document.querySelectorAll("[data-dropdown].active").forEach(dropdown => {
+        if (dropdown === CurrentDropdown)
+            return
+
+        dropdown.classList.remove('active')
+    })
+})
